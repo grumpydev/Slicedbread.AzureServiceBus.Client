bf430f4283ee8857794aeeae683a84329b4111b4
Steven Robbins <ste.robbins@gmail.com>
2015-10-06 21:46:27 +0100

    Added initial abstracton over the official service bus client

---
+++ src/Slicedbread.AzureServiceBus.Client/ServiceBus/IServiceBus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Slicedbread.AzureServiceBus.Client.ServiceBus
+{
+    public interface IServiceBus : IDisposable
+    {
+        void Connect(string connectionString, string queueName);
+
+        void VerifyQueue(string connectionString, QueueDescription description);
+
+        Task Send(string messageType, Stream payload);
+
+        void OnMessageAsync(Func<BrokeredMessage, Task> callback, OnMessageOptions options);
+    }
+}
---
+++ src/Slicedbread.AzureServiceBus.Client/ServiceBus/ServiceBus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Slicedbread.AzureServiceBus.Client.ServiceBus
+{
+    public class ServiceBus : IServiceBus
+    {
+        private QueueClient _client;
+
+        public void Connect(string connectionString, string queueName)
+        {
+            this._client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+        }
+
+        public void VerifyQueue(string connectionString, QueueDescription description)
+        {
+            var namespaceManager =
+                NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.QueueExists(description.Path))
+            {
+                namespaceManager.CreateQueue(description);
+            }
+        }
+
+        public async Task Send(string messageType, Stream payload)
+        {
+            if (this._client == null)
+            {
+                throw new InvalidOperationException("Client is not connected.");
+            }
+
+            var message = new BrokeredMessage(payload, true) { ContentType = messageType }; 
+
+            await this._client.SendAsync(message);
+        }
+
+        public void OnMessageAsync(Func<BrokeredMessage, Task> callback, OnMessageOptions options)
+        {
+            if (this._client == null)
+            {
+                throw new InvalidOperationException("Client is not connected.");
+            }
+
+            this._client.OnMessageAsync(callback, options);
+        }
+
+        public void Dispose()
+        {
+            if (this._client != null)
+            {
+                this._client.Close();
+            }
+        }
+    }
+}
