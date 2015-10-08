93eafd22807fe128e6357febae5c32530cbf9642
Steven Robbins <ste.robbins@gmail.com>
2015-10-08 17:16:40 +0100

    Added basic demo apps

---
+++ src/Slicedbread.AzureServiceBus.Demo.Receiver/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Slicedbread.AzureServiceBus.Client;
+
+namespace Slicedbread.AzureServiceBus.Demo.Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter connectionstring: ");
+            var connectionString = Console.ReadLine();
+            Console.Write("Enter queue name: ");
+            var queueName = Console.ReadLine();
+
+            var listener = new ServiceBusListener(new[] {new BusSubscriber()});
+
+            listener.Connect(connectionString, queueName);
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+    internal class BusSubscriber : IDynamicSubscriber
+    {
+        public bool CanProcess(MessageMetadata metadata)
+        {
+            return true;
+        }
+
+        public Task Process(MessageMetadata metadata, dynamic body)
+        {
+            Console.WriteLine("++++++");
+            Console.WriteLine("Message type: " + metadata.MessageType);
+            Console.WriteLine("Foo: " + body.Foo);
+            Console.WriteLine("Bar: " + body.Bar);
+            Console.WriteLine("++++++");
+            Console.WriteLine();
+
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/Slicedbread.AzureServiceBus.Demo.Sender/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Slicedbread.AzureServiceBus.Client;
+
+namespace Slicedbread.AzureServiceBus.Demo.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter connectionstring: ");
+            var connectionString = Console.ReadLine();
+            Console.Write("Enter queue name: ");
+            var queueName = Console.ReadLine();
+
+            var client = new ServiceBusClient();
+
+            client.Connect(connectionString, queueName);
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Enter message type: ");
+                var messageType = Console.ReadLine();
+                Console.Write("Enter value for foo: ");
+                var foo = Console.ReadLine();
+                Console.Write("Enter value for bar: ");
+                var bar = Console.ReadLine();
+                client.Send(messageType, new { Foo = foo, Bar = bar }).Wait();
+                Console.WriteLine();
+            }
+        }
+    }
+}
